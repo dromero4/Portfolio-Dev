0c75adb1050f5047efbfbc7e7e7e89024dcb2cfe
David Romero <d.romero@sapalomera.cat>
2026-06-17 22:33:48 +0200

    Add tech section and related data; update Hero and Header styles

---
+++ src/utils/tecnologias.js
@@ -0,0 +1,42 @@
+export const TECNOLOGIAS = [
+    {
+        title: "JavaScript",
+        icon: "images/dev/JavaScript.webp"
+    },
+    {
+        title: "TypeScript",
+        icon: "images/dev/TypeScript.webp"
+    },
+    {
+        title: "PHP",
+        icon: "images/dev/PHP.webp"
+    },
+    {
+        title: "TailwindCSS",
+        icon: "images/dev/TailwindCSS.png"
+    },
+    {
+        title: "NodeJS",
+        icon: "images/dev/NodeJS.png"
+    },
+    {
+        title: "React",
+        icon: "images/dev/React.webp"
+    },
+    {
+        title: "Astro",
+        icon: "images/dev/Astro.webp"
+    },
+    {
+        title: "NextJS",
+        icon: "images/dev/NextJS.webp"
+    },
+    {
+        title: "Prisma",
+        icon: "images/dev/Prisma.webp"
+    },
+    {
+        title: "MySQL",
+        icon: "images/dev/MySQL.webp"
+    }
+]
